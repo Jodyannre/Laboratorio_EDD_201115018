54d005ed7ec0ff53585e92d01be8f84bea08341e
Marvin <marvin1ronal@gmail.com>
2020-08-09 13:25:09 -0600

    Calificacion Tarea 1

---
+++ Tarea1/main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "ListaC.h"
+#include "NodoC.h"
+int main(){
+    NodoA* a=newNodoC(1);
+    NodoA* b=newNodoC(2);
+    NodoC* c=newNodoC(3);
+    NodoC* d=newNodoC(4);
+    NodoC* e=newNodoC(5);
+    NodoC* f=newNodoC(6);
+    NodoC* g=newNodoC(7);
+    NodoC* error=newNodoC(-1000);    
+
+    ListaC* l=nuevaLista();
+    add(l,a);
+    add(l,b);
+    add(l,c);
+    add(l,d);
+    add(l,e);
+    add(l,f);
+    add(l,g);
+    add(l,error);
+    eliminar(1,l);
+    eliminar(1,l);
+    eliminar(1,l);
+    eliminar(1,l);
+    eliminar(1,l);
+    eliminar(1,l);
+    eliminar(1,l);
+    //getNodo(2,l);
+    imprimir(l);
+    generar(l);
+    Liberar(l);
+    free(l);
+    return 0;
+}
